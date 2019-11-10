89f034100fe79fd23e336c0f247762b2503b83d6
david <davidvoler@gmail.com>
2019-11-10 17:11:34 +0200

    implementing templates

--- src/template.py
+++ src/template.py
@@ -1,6 +1,6 @@
 from string import Template
 module_head = Template('''
-(ns $ns
+(ns $namespace
   ""
   (:require [libpython-clj.python
              :refer [import-module
@@ -55,29 +55,52 @@ method_positional_kw_defaults = Template('''
 def get_module_head(namespace, module_name):
   return module_head.substitute({"namespace":namespace, "module_name":module_name})
 
-def get_function(module_name, positional_args=None, kw_args=None, defaults=None, docstring=""):
+def get_function(module_name, function_name, positional_args=None, kw_args=None, defaults=None, docstring=""):
   if positional_args and kw_args and defaults:
-    return method_positional_kw_defaults.replace({
+    return method_positional_kw_defaults.substitute({
+      "docstring":docstring,
+      "function_name":function_name,
+      "module_name":module_name,
       "positional_args":positional_args,
       "kw_args":kw_args,
       "defaults":defaults
     })
   elif positional_args and kw_args:
-    return method_positional_kw.replace({
+    return method_positional_kw.substitute({
+      "module_name":module_name,
+      "function_name":function_name,
+      "docstring":docstring,
       "positional_args":positional_args,
       "kw_args":kw_args
     })
   elif  defaults and kw_args:
-    return method_kw_defaults.replace({
+    return method_kw_defaults.substitute({
+      "module_name":module_name,
+      "function_name":function_name,
+      "docstring":docstring,
       "positional_args":positional_args,
       "kw_args":kw_args
     })
   elif kw_args:
-    return method_kw.replace({
+    return method_kw.substitute({
+      "module_name":module_name,
+      "function_name":function_name,
+      "docstring":docstring,
       "kw_args":kw_args
     })
   elif positional_args:
-    return method_positional.replace({
+    return method_positional.substitute({
+      "module_name":module_name,
+      "function_name":function_name,
+      "docstring":docstring,
       "positional_args":positional_args
     })
 
+
+
+if __name__ == "__main__":
+  print(get_module_head("keras", "keras"))
+  print(get_function("keras", "input", positional_args="a b c"))
+  print(get_function("keras", "input", positional_args="a b c", kw_args = "d e f"))
+  print(get_function("keras", "input", positional_args="a b c", kw_args = "d e f", defaults="e 1 f 2"))
+  print(get_function("keras", "input", positional_args="a b c",docstring="some docstring"))
