cac2089c847653516058327f00d2c07cf853275f
david <davidvoler@gmail.com>
2019-11-23 10:41:20 +0200

    solving path

--- src/writer.py
+++ src/writer.py
@@ -4,11 +4,13 @@ from version import VERSION
 
 def save_file(src_path, path, filename, data):
   mkpath(os.path.join(src_path,path))
+
   with open(os.path.join(src_path, path, filename), "w") as f:
     for l in data:
       f.writelines(l)
 
 def make_src_path(base_path, module_name, version):
+
   path = os.path.join(base_path, module_name,
                         f"pyclj-{VERSION}-{module_name}{version}")
   if os.path.exists(path):
@@ -18,11 +20,8 @@ def make_src_path(base_path, module_name, version):
             break
         except:
             pass
-  # print(base_path, module_name, version)
-  # print(path)
-  project_path = mkpath(path)[0]
-  print(project_path)
-  src_path = mkpath(os.path.join(project_path,"src"))[0]
+  project_path = mkpath(path)
+  src_path = mkpath(os.path.join(path,"src"))[0]
   print(project_path, src_path)
   # exit(0)
-  return project_path, src_path
+  return path, src_path
