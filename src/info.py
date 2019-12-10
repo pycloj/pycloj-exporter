d2214b165f57c66fee4b158fe73b068dfaad25d3
david <davidvoler@gmail.com>
2019-12-10 17:19:50 +0200

    adding class handler

--- src/info.py
+++ src/info.py
@@ -3,8 +3,13 @@ import argparse
 import pkgutil
 
 
+def protect_docstring(dstr):
+    if dstr and type(dstr) == str:
+        return dstr.replace('"', '\\"')
+    else:
+        return ""
+
 def import_all_sub_module(base_module_name):
-    import pkgutil
     the_module = __import__(base_module_name)
     globals()[base_module_name] = the_module
     for imp, m, ispkg in pkgutil.walk_packages(path=the_module.__path__):
@@ -15,6 +20,7 @@ def import_all_sub_module(base_module_name):
                 globals()[mname] = sub_m
             except:
                 print(f"could not import module {m}")
+
 
 
 def import_module(module_name, import_sub_module=True):
@@ -35,29 +41,89 @@ def import_module(module_name, import_sub_module=True):
 
 
 def get_functions(the_module):
-  function_list = []
-  members = inspect.getmembers(the_module)
-  for member in members:
-    if inspect.isfunction(member[1]):
-        function_list.append(member[0])
-  return function_list
+    function_list = []
+    members = inspect.getmembers(the_module)
+    for member in members:
+        if inspect.isfunction(member[1]):
+            function_list.append(member[0])
+    return function_list
+
 
 def get_classes(the_module):
-  class_list = []
-  members = inspect.getmembers(the_module)
-  for member in members:
-    if inspect.isclass(member[1]):
-        class_list.append(member[0])
-  return class_list
+    class_list = []
+    members = inspect.getmembers(the_module)
+    for member in members:
+        if inspect.isclass(member[1]):
+            class_list.append(member[0])
+    return class_list
+
 
 def get_submodule(the_module):
-  submodule_list = []
-  members = inspect.getmembers(the_module)
-  for member in members:
-    if inspect.ismodule(member[1]):
-        submodule_list.append(member[0])
-  return submodule_list
+    submodule_list = []
+    members = inspect.getmembers(the_module)
+    for member in members:
+        if inspect.ismodule(member[1]):
+            submodule_list.append(member[0])
+    return submodule_list
 
+def empty_or_val(val):
+  if val == inspect._empty:
+    return None
+  return val
+
+def king_to_str(kind):
+  return str(kind).split(".")[-1]
+def get_function_info(fn):
+    args = []
+    sig = inspect.signature(fn)
+    for p in sig.parameters.values():
+        args.append({
+            "name": empty_or_val(p.name),
+            "empty": empty_or_val(p.empty),
+            "kind": king_to_str(p.kind),
+            "default": empty_or_val(p.default),
+            "annotation": empty_or_val(p.annotation),
+        })
+    return {
+        "args": args,
+        "name": fn.__name__,
+        "return_annotation": empty_or_val(sig.return_annotation),
+        "doc": protect_docstring(fn.__doc__)
+    }
+
+# import keras
+# print(get_function_info(keras.Input))
+
+def get_class_members(the_class):
+    class_members = []
+    class_functions = []
+    class_methods = []
+    class_properties = []
+    members = inspect.getmembers(the_class)
+    for member in members:
+      if inspect.isfunction(member[1]):
+        class_functions.append(get_function_info(member[1]))
+      elif inspect.ismethod(member[1]):
+        class_methods.append(get_function_info(member[1]))
+      elif isinstance(member[1], property):
+        class_properties.append(member[0])
+      else:
+        class_members.append(member[0])
+    
+    return {
+      "module":the_class.__module__,
+      "name":the_class.__name__,
+      "members": class_members,
+      "methods": class_methods,
+      "functions": class_functions,
+      "properties": class_properties,
+      "doc": protect_docstring(the_class.__doc__)
+    }
+
+
+
+import keras
+print(get_class_members(keras.Sequential))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Info about a python module ")
@@ -71,33 +137,27 @@ if __name__ == "__main__":
                         type=bool,
                         help="should we import submodules",
                         default=True)
-    parser.add_argument(
-        '--data',
-        default='all',
-        choices=['submodules', 'functions', 'classes', 'all'],
-        help='the kind of data we want for the element')
+    parser.add_argument('--data',
+                        default='all',
+                        choices=['submodules', 'functions', 'classes', 'all'],
+                        help='the kind of data we want for the element')
 
     args = parser.parse_args()
     the_module = import_module(args.module)
 
-
     if args.data == "submodules":
-      print("--submodules--")
-      print(get_submodule(the_module))
+        print("--submodules--")
+        print(get_submodule(the_module))
     if args.data == "functions":
-      print("--functions--")
-      print(get_functions(the_module))
+        print("--functions--")
+        print(get_functions(the_module))
     if args.data == "classes":
-      print("--classes--")
-      print(get_classes(the_module))
+        print("--classes--")
+        print(get_classes(the_module))
     if args.data == "all":
-      print("--submodules--")
-      print(get_submodule(the_module))
-      print("--functions--")
-      print(get_functions(the_module))
-      print("--classes--")
-      print(get_classes(the_module))
-
-
-
-
+        print("--submodules--")
+        print(get_submodule(the_module))
+        print("--functions--")
+        print(get_functions(the_module))
+        print("--classes--")
+        print(get_classes(the_module))
