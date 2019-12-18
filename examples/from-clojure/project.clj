ebe1843d0a7f87b85503533c6600f5e87b663658
daslu <daniel.slutsky@gmail.com>
2019-12-18 20:24:59 +0200

    deps version

--- examples/from-clojure/project.clj
+++ examples/from-clojure/project.clj
@@ -4,5 +4,5 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [cnuernber/libpython-clj "1.21"]]
+                 [cnuernber/libpython-clj "1.28"]]
   :repl-options {:init-ns from-clojure.core})
